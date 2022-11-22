dc2ebc908f058935636d2df324c03d3f7764a9d1
Adrian S <a.p.szydlowski@gmail.com>
2022-11-22 20:48:24 +0100

    tested removeAllEntities

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -1,20 +1,38 @@
 package pl.sda;
 
+import pl.sda.dao.RemoveFromDB;
+import pl.sda.dao.SentToDB;
 import pl.sda.gui.Gui;
+import pl.sda.models.GsonMapper;
+import pl.sda.models.JsonNode;
+import pl.sda.utils.ApplicationPropertiesProvider;
+import pl.sda.utils.FileReader;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
 
+
+
         while (true) {
             Gui.showMenu();
             int choice = Gui.getUserChoice();
             if (choice == 4) {
+                RemoveFromDB.removeAllEntitiesFromDBAfterWork(ApplicationPropertiesProvider.getSql7578590Properties());
                 break;
             }
             BusinessLogic.handle(choice);
         }
+    }
+    private static void sendAllJsonDatatoDB() throws IOException {
+        FileReader fileReader = new FileReader();
+        File file = fileReader.readFile();
+        GsonMapper gsonMapper = new GsonMapper();
+        List<JsonNode> gsonJson = gsonMapper.fromJson(file);
+        SentToDB.sentFromJSonToDB(ApplicationPropertiesProvider.getSql7578590Properties(), gsonJson);
     }
 }
